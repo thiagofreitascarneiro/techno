b2516e32b8de78669b5ecbcac517c064054c508c
Thiago <thiagofreitascarneiro@hotmail.com>
2022-09-30 15:55:50 -0300

    criando modal, salvando dados em localStorage e criando alertas

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ const vm = new Vue({
     data: {
         produtos: [],
         produto: false,
+        carrinho: [],
+        mensagemAlerta: 'item adicionado',
+        alertaAtivo: false,
     },
     filters: {
         numeroPreco(valor) {
             return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"});
+        }
+    },
+    computed: {
+        carrinhoTotal() {
+            let total = 0;
+            console.log('caiu aqui')
+            if(this.carrinho.length) {
+                this.carrinho.forEach(item => {
+                    console.log(item)
+                    total += item.preco;
+                })   
+            }
+            return total;
         }
     },
     methods: {
@@ -37,12 +53,41 @@ const vm = new Vue({
                 this.produto = false;
             }
             // console.log(target);
-            // console.log(currentTarget)
+            // console.log(currentTarget)       
+        },
+        adicionarItem() {        
+            this.produto.estoque--;
+            const { id, nome, preco } = this.produto;
+            this.carrinho.push({ id, nome, preco });
+            this.alerta(`${nome} adicionado ao carrinho.`);
             
+        },
+
+        removerItem(index) {
+            this.carrinho.splice(index, 1);
+        },
+        checarLocalStorage() {
+            if(window.localStorage.carrinho) {
+                this.carrinho = JSON.parse(window.localStorage.carrinho);
+            }
+        }, 
+        alerta(mensagem) {
+            this.mensagemAlerta = mensagem;
+            this.alertaAtivo = true;
+            setTimeout(() => {
+                this.alertaAtivo = false;
+            }, 1500);
         }
+        },
+        watch: {
+            carrinho() {
+                window.localStorage.carrinho = JSON.stringfy(this.carrinho);
+                
+            }
         },
 
         created() {
             this.fetchProdutos();
-        } 
+            checarLocalStorage();
+        }, 
 })
